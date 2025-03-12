a1a0fbba04baa2fe362b0e9928b9919e5b33d8ea
selimfirat <yilmazselimfirat@gmail.com>
2025-03-12 01:11:27 +0000

    Refactor: remove unused imports for Yilmaz2023 DeepJSCC-NOMA components

--- kaira/models/image/__init__.py
+++ kaira/models/image/__init__.py
@@ -6,8 +6,6 @@ This module provides models specifically designed for image data transmission.
 from .tung2022_deepjscc_q import Tung2022DeepJSCCQEncoder, Tung2022DeepJSCCQDecoder, Tung2022DeepJSCCQ2Encoder, Tung2022DeepJSCCQ2Decoder
 from .yang2024_deepjcc_swin import Yang2024DeepJSCCSwinEncoder, Yang2024DeepJSCCSwinDecoder
 from .yilmaz2023_deepjscc_noma import Yilmaz2023DeepJSCCNOMA
-from .yilmaz2023_deepjscc_noma_encoder import Yilmaz2023DeepJSCCNOMAEncoder
-from .yilmaz2023_deepjscc_noma_decoder import Yilmaz2023DeepJSCCNOMADecoder
 
 __all__ = [
     "Tung2022DeepJSCCQEncoder",
